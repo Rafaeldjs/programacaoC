45733ddb13e8a9b8300d96629346b30c61d60efd
Rafaeldjs <rafa01.ads@gmail.com>
2025-11-20 11:18:12 -0300

    Add user input and output functionality

--- entrada_e_saida_de_dados.c
+++ entrada_e_saida_de_dados.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    char nome[30];
+    char cidade[30];
+    int idade;
+    
+    printf("Digite seu nome:\n");
+    scanf("%s", nome);
+    
+    printf("Qual a sua idade:\n");
+    scanf("%d", &idade);
 
+    printf("Digite onde voçê mora:\n");
+    scanf("%s", cidade);
+
+
+    printf("seu nome e:%s\n", nome);
+    printf("Voçê tem:%d anos\n", idade);
+    printf("Mora em:%s\n", cidade);
+
+    return 0;
+}
